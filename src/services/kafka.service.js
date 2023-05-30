6c5d377925f579580875b364e99145524efb4677
ChristianReyes11 <christianamaro2000@gmail.com>
2023-05-30 10:55:53 -0600

    Add repo

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -1,24 +1,39 @@
-class KafkaService {
-    //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
-    url = 'https://nodeproducer-service-christianreyes11.cloud.okteto.net/';
-   
-    reaction = async (name) => {
-     await fetch(this.url + 'like?name=' + name, {
-        method: 'GET',
-        headers: {
-           'Content-type': 'application/json; charset=UTF-8',
-        },  
-     })  
-        .then((response) => console.log(response.json()))
-        .then((data) => {
-          console.log(data);
-        })  
-        .catch((err) => {
-           console.log(err.message);
-        }); 
-    }
-  
-  }
-  
-  const kafkaService = new KafkaService();
-  export default kafkaService;
+  class KafkaService {
+   // url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
+   url = 'https://nodeproducer-service-christianreyes11.cloud.okteto.net/';
+ 
+   reaction = async (userid, objectid, reactionid) => {
+     await fetch(this.url + '/reactions?userid=' + userid+'&objectid='+objectid +'&reactionid='+reactionid, {
+       method: 'GET',
+       headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+       },
+     })
+       .then((response) => response.json())
+       .then((data) => {
+         console.log(data);
+       })
+       .catch((err) => {
+         console.log(err.message);
+       });
+   };
+ 
+   comment = async (userid, objectid, message) => {
+     await fetch(this.url + '/comments?userid=' + userid+'&objectid='+objectid +'&message='+message, {
+       method: 'GET',
+       headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+       },
+     })
+       .then((response) => response.json())
+       .then((data) => {
+         console.log(data);
+       })
+       .catch((err) => {
+         console.log(err.message);
+       });
+   };
+ }
+ 
+ const kafkaService = new KafkaService();
+ export default kafkaService;
